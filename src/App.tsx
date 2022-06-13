238cdcfbd8f8ff04416e5de3e60a876acaae0356
MelSept <septurmelina@gmail.com>
2022-06-13 17:39:08 -0300

    Se crea un fetch de datos y finaliza el proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import List from "./components/List";
 import Form from "./components/Form";
-import { Sub } from "./types";
+import { Sub, SubsResponseFromApi } from "./types";
 
 // useRef es un hook que guarda un valor sin renderizar
 // un state puede tener varios estados, por lo que re comienda separlos ya que pueden ir cambiando y de esta
@@ -14,20 +14,6 @@ interface AppState {
   newSubsNumber: number;
 }
 
-// Estado inicial del array
-const INITIAL_STATE = [
-  {
-    nick: "dapelu",
-    subMonths: 3,
-    avatar: "https://i.pravatar.cc/150?u=dapelu",
-    description: "Dapelu hace de moderador a veces",
-  },
-  {
-    nick: "sergio_serrano",
-    subMonths: 7,
-    avatar: "https://i.pravatar.cc/150?u=sergio_serrano",
-  },
-];
 // Los array en typescript son parametrizables
 // en este ejemplo useState recibe el array de tipo subs
 function App() {
@@ -39,18 +25,46 @@ function App() {
   //le pasamos lo que queremos guardar en divRef en este caso el HTMLdivElement
   const divRef = useRef<HTMLDivElement>(null);
 
+  //como realizar un fecth de datos
   useEffect(() => {
-    setSubs(INITIAL_STATE);
+    const fetchSubs = (): Promise<SubsResponseFromApi> => {
+      return fetch("http://localhost:3001/subs").then((res) => res.json());
+    };
+
+    const mapFromApiToSubs = (apiResponse: SubsResponseFromApi): Array<Sub> => {
+      return apiResponse.map((subFromApi) => {
+        const {
+          months: subMonths,
+          profileUrl: avatar,
+          nick,
+          description,
+        } = subFromApi;
+
+        return {
+          nick,
+          description,
+          avatar,
+          subMonths,
+        };
+      });
+    };
+
+    //devuelve una promesa donde tenemos mapFromApiToSubs el cual recibe un parametro y le llega como argumento
+    // a setSubs
+
+    fetchSubs().then(mapFromApiToSubs).then(setSubs);
   }, []);
 
   const handleNewSub = (newSub: Sub): void => {
     setSubs((subs) => [...subs, newSub]);
+    setNewSubsNumber((n) => n + 1);
   };
 
   return (
     <div className="App" ref={divRef}>
       <h1>Mis Subs</h1>
       <List subs={subs} />
+      New subs: {newSubsNumber}
       <Form onNewSub={handleNewSub} />
     </div>
   );
